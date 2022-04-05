28ae97ac467e4c8bd0e4de14594e2188c9c955c5
Jaine Kiefer <jainekiefer013@gmail.com>
2022-04-04 22:26:50 -0300

    master

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{backgroundColor: "blue", height:"100px"}}>
+    
     </View>
+//https://reactnative.dev/docs/flexbox
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
