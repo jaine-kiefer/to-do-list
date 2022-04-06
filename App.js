e0a2a2e8b47d7c7516d8d86c7e6b541f6af6e7a6
Jaine Kiefer <jainekiefer013@gmail.com>
2022-04-05 21:39:08 -0300

    master

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{backgroundColor: "blue", height:"100px"}}>
-    
+    <Text>Primeiro Aplicativo</Text>
     </View>
 //https://reactnative.dev/docs/flexbox
   );
