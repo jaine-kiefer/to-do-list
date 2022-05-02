47c3afe37b65d386e21ad586f0348bfcee45a577
Jaine Kiefer <jainekiefer013@gmail.com>
2022-05-02 19:43:25 -0300

    título, input e botão

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
 
 export default function App() {
+  let [texto, setaTexto] = useState("Jaine");
   return (
-    <View style={{backgroundColor: "blue", height:"100px"}}>
-    <Text>Primeiro Aplicativo</Text>
+    <View>
+      <View> 
+        <Text style={style.titulo}>Lista de Tarefas da {texto}</Text>
+      </View>
+      <View>
+        <TextInput style={style.input} value={texto} onChangeText={setaTexto}/>
+        <Button title='Adicionar tarefa'/>
+      </View>
     </View>
-//https://reactnative.dev/docs/flexbox
   );
 }
 
+const style = StyleSheet.create({
+  titulo: {
+    fontSize: 24,
+    textAlign: "center",
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+    padding: 8,
+  }
+})
